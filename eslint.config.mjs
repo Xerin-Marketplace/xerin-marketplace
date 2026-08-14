170d1de58ad0e9c157bb79fe4c49884a72fe2927
AdamMashaka <mashakaadam123@gmail.com>
2026-08-14 17:57:15 +0300

    package conflict

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,6 @@ import { FlatCompat } from "@eslint/eslintrc";
 const compat = new FlatCompat({
   baseDirectory: import.meta.dirname,
 });
-
 export default [
   {
     ignores: [".next/**", "node_modules/**", "public/**"],
